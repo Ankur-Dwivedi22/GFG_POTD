bf2c9fa1facfc59adab9d3d250b8e0b4e6169d68
Ankur-Dwivedi22 <ankur.2201005cs@iiitbh.ac.in>
2026-07-06 17:05:39 +0530

    Arrays

---
+++ Arrays/Max Sum Path in Two Arrays.cpp
@@ -0,0 +1,40 @@
+class Solution {
+  public:
+    int maxPathSum(vector<int> &a, vector<int> &b) {
+        // Code here
+        int n = a.size() , m = b.size();
+        int i = 0, j = 0;
+        int s1 = 0, s2 = 0, ans = 0;
+        while(i < n && j < m){
+            if(a[i] < b[j]){
+                s1 += a[i];
+                i++;
+            }
+            else if(a[i] > b[j]){
+                s2 += b[j];
+                j++;
+            }
+            else{
+                ans += max(s1, s2);
+                s1 = 0;
+                s2 = 0;
+                ans += a[i];
+                i++;
+                j++;
+            }
+        }
+        
+        while(i < n){
+            s1 += a[i];
+            i++;
+        }
+        
+        while(j < m){
+            s2 += b[j];
+            j++;
+        }
+        
+        ans += max(s1, s2);
+        return ans;
+    }
+};
